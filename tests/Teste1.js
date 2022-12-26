1573b9ce4c784170bab0f2b77df214bb6a259e43
BinaryCatto <121410251+BinaryCatto@users.noreply.github.com>
2022-12-25 23:05:30 -0300

    Update Teste1.js

--- tests/Teste1.js
+++ tests/Teste1.js
@@ -1,7 +1,3 @@
-const chai = require('chai');
-const assert = chai.assert;
-
-    test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
-    });
+test('#isNull, #isNotNull', function () {
+    assert.isNull(null, 'This is an optional error description - e.g. null is null');
+    assert.isNotNull(1, '1 is not null');
